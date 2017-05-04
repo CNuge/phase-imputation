2bd1600378d4ebf461a2f49903bdbda62def007f
WATTBA <nugentc@uoguelph.ca>
2017-05-04 15:43:24 -0400

    beginning the algorithm writing

--- Example_phase_hq.f
+++ Example_phase_hq.f
@@ -1,21 +1,19 @@
-chrom                        test_phase_lg
-
-C1_m11                  H,A,H,A,H,A,A,A,A,A,H,H,H,H,H,H,A,A,H,H,A,H,A,H,H,A,A,A,H,H,H,H,-,A,A,H,H,H,A,H,A,-,A,A,A,H,A,A,H,H,A,A,A,H,A,H,A,A,A,A,H,H,H,H,H,A,H,A,A,A,A,A,-,H,A,H,A,H,H,H,A,A,H,A,H
-C1_m21                  H,A,H,A,H,-,A,A,-,A,H,-,H,H,H,-,-,A,H,H,A,H,A,H,H,A,-,A,-,H,H,H,A,A,A,H,H,H,A,H,A,H,A,A,A,H,A,A,-,H,A,A,-,H,A,H,A,A,A,A,H,H,-,H,H,A,H,A,A,A,A,A,H,H,A,H,A,H,H,H,A,A,H,A,H
-C1_m31                  H,A,H,A,H,A,A,A,A,A,H,H,H,-,H,H,A,A,H,H,A,H,A,H,-,A,A,A,H,H,H,H,A,A,A,H,H,H,A,H,-,H,A,A,A,H,A,A,H,H,-,A,A,H,-,H,A,A,H,A,H,H,A,H,H,A,H,A,A,A,A,A,H,H,A,H,A,H,H,H,A,A,H,A,-
-C1_m41                  H,A,H,-,H,A,A,A,A,A,H,H,H,H,H,H,-,A,H,H,A,H,A,H,H,A,A,A,-,H,H,H,A,A,A,-,H,H,A,H,A,H,A,A,A,H,A,A,H,H,A,A,A,-,A,H,-,A,-,A,H,-,H,H,H,A,-,A,A,A,A,A,H,H,A,H,A,H,H,H,A,A,H,A,H
-C1_m51                  H,A,H,A,H,A,A,A,A,A,H,H,H,H,H,H,A,A,H,H,A,H,A,H,H,A,-,A,-,H,H,H,A,A,A,H,H,H,A,H,A,H,A,A,A,H,A,A,H,H,A,A,A,H,A,H,A,A,A,A,H,H,-,H,H,A,H,A,A,A,A,A,H,H,A,H,A,H,H,H,A,-,H,A,H
-X2_m12                   H,A,H,A,H,A,A,A,A,A,H,H,H,H,H,H,A,A,H,H,A,H,A,H,H,A,A,A,H,H,A,H,A,A,A,H,H,H,A,H,A,H,A,A,A,H,A,A,H,H,A,A,A,H,A,H,A,A,A,A,A,H,H,H,H,A,H,A,A,A,A,A,H,H,A,A,A,H,H,H,A,A,H,A,H
-C3_m13                 H,A,H,A,H,-,A,A,A,A,H,H,A,H,H,H,A,A,-,A,A,H,A,H,H,A,A,A,H,H,A,H,A,A,A,H,H,H,A,H,A,H,A,A,A,H,A,A,H,H,A,A,A,H,A,H,-,A,H,A,A,H,H,H,H,A,H,A,A,A,A,A,H,H,A,A,A,H,H,H,A,A,H,A,H
-C3_m23                  H,A,H,A,H,-,A,A,A,A,H,H,A,H,H,H,A,A,-,A,A,H,A,H,H,A,A,A,-,H,A,H,A,A,A,H,H,H,A,H,A,H,A,A,A,H,A,A,H,-,A,A,A,H,-,H,A,A,A,A,A,H,H,H,H,A,H,A,A,A,A,A,H,H,A,-,A,H,H,H,A,A,H,A,H
-C3_m33                  H,A,H,A,H,-,A,A,A,A,H,H,A,H,H,H,A,A,-,A,A,H,A,H,H,A,A,A,H,-,A,H,A,A,A,H,H,H,A,H,A,H,A,A,A,H,A,A,-,-,A,A,A,H,-,H,A,A,A,-,A,H,H,H,H,A,H,A,A,A,A,A,H,-,A,A,A,H,H,H,A,A,H,A,H
-C3_m43                  H,A,H,A,H,-,A,A,A,A,H,H,A,H,H,H,A,A,-,A,A,H,A,H,H,A,A,A,H,H,A,H,A,A,A,H,H,H,A,H,A,H,A,A,A,H,A,-,H,A,H,A,A,H,H,H,A,A,A,A,A,H,H,H,H,A,H,A,A,A,A,A,H,H,A,A,A,H,H,H,A,A,H,A,H
-X4_m14                H,A,H,A,H,A,A,A,A,A,H,H,A,H,H,H,A,A,H,H,A,H,A,H,H,A,A,A,H,H,A,H,A,A,A,-,H,H,A,-,A,H,A,A,A,H,A,A,H,H,A,A,A,H,A,H,A,A,A,-,A,H,H,H,H,A,H,A,A,A,A,A,H,H,A,A,A,H,H,A,-,A,H,A,H
-X4_m24                H,A,-,A,-,A,-,A,A,A,-,H,-,H,H,H,A,A,H,H,A,H,A,H,H,A,A,A,H,H,A,H,A,A,A,H,H,H,A,-,A,H,A,A,A,H,A,A,H,H,A,A,A,H,A,H,A,-,A,-,A,H,H,H,H,A,H,A,A,A,A,A,H,H,A,A,A,H,H,A,A,A,H,A,H
-S5_m15                    H,A,H,A,H,A,A,A,A,A,H,H,A,H,-,H,A,A,A,H,A,H,A,H,H,A,A,A,H,H,H,H,A,A,A,H,H,H,A,H,A,-,A,A,-,H,A,A,H,H,A,A,A,H,A,H,A,A,A,A,A,H,H,H,H,A,H,A,A,A,A,A,H,H,A,A,A,H,H,A,A,A,H,A,H
-X6_m16                    H,A,H,A,H,A,A,A,A,A,H,H,A,H,H,H,A,-,H,H,A,H,A,H,H,A,A,A,H,H,H,H,A,A,A,H,H,H,A,H,A,H,A,A,-,H,A,A,H,H,A,A,A,H,H,H,A,A,A,A,A,H,H,H,H,H,H,A,A,A,A,A,H,H,A,A,A,H,H,-,A,A,H,A,H
-X6_m26                    H,A,H,A,H,-,A,A,-,A,H,H,A,-,H,H,A,-,H,H,A,H,A,H,H,A,A,A,H,H,H,H,A,A,-,H,-,H,-,H,A,-,A,A,-,H,A,A,H,H,A,A,A,H,H,H,A,A,-,A,A,H,H,H,-,H,H,A,A,-,A,-,H,-,A,A,A,H,-,A,A,A,H,A,H
-X6_m36                    H,A,H,A,H,A,A,A,A,A,H,-,A,H,H,H,A,-,H,H,A,H,A,H,H,A,A,A,H,H,H,H,A,A,A,H,H,-,A,H,A,H,A,A,-,H,A,A,H,H,A,A,A,H,H,H,A,A,A,A,A,H,-,H,H,H,H,A,A,A,A,A,H,H,A,A,A,H,H,A,A,A,H,A,H
-X6_m46                    H,A,H,A,H,A,A,A,A,A,H,H,A,H,H,H,A,-,H,H,A,H,A,H,H,A,A,A,H,H,H,H,A,A,A,H,H,H,A,H,A,-,A,A,-,H,A,A,H,H,A,A,A,H,H,H,A,A,A,A,-,H,H,H,H,H,H,A,A,A,A,A,H,H,A,A,A,H,H,A,A,A,H,A,H
-S7_m17                    H,A,H,A,H,A,A,A,A,A,H,H,A,H,H,H,A,A,H,-,A,H,A,H,H,A,A,A,H,H,A,H,A,A,A,H,H,H,A,H,A,H,A,A,A,H,A,A,H,H,A,A,A,H,A,H,A,A,A,A,A,H,H,H,H,H,H,A,A,-,A,A,H,H,A,A,A,H,H,H,A,A,H,A,H
-S8_m18                    H,A,H,H,H,A,A,A,A,H,H,H,A,A,H,H,A,A,H,H,A,H,A,H,H,A,A,A,H,H,A,A,A,A,A,H,H,H,A,H,A,H,H,H,A,H,A,A,H,H,A,A,H,H,A,H,A,A,A,A,A,H,H,H,H,H,H,A,H,A,A,A,H,H,A,A,A,H,H,H,H,A,H,A,A
+C1_m11,H,A,H,A,H,A,A,A,A,A,H,H,H,H,H,H,A,A,H,H,A,H,A,H,H,A,A,A,H,H,H,H,-,A,A,H,H,H,A,H,A,-,A,A,A,H,A,A,H,H,A,A,A,H,A,H,A,A,A,A,H,H,H,H,H,A,H,A,A,A,A,A,-,H,A,H,A,H,H,H,A,A,H,A,H
+C1_m21,H,A,H,A,H,-,A,A,-,A,H,-,H,H,H,-,-,A,H,H,A,H,A,H,H,A,-,A,-,H,H,H,A,A,A,H,H,H,A,H,A,H,A,A,A,H,A,A,-,H,A,A,-,H,A,H,A,A,A,A,H,H,-,H,H,A,H,A,A,A,A,A,H,H,A,H,A,H,H,H,A,A,H,A,H
+C1_m31,H,A,H,A,H,A,A,A,A,A,H,H,H,-,H,H,A,A,H,H,A,H,A,H,-,A,A,A,H,H,H,H,A,A,A,H,H,H,A,H,-,H,A,A,A,H,A,A,H,H,-,A,A,H,-,H,A,A,H,A,H,H,A,H,H,A,H,A,A,A,A,A,H,H,A,H,A,H,H,H,A,A,H,A,-
+C1_m41,H,A,H,-,H,A,A,A,A,A,H,H,H,H,H,H,-,A,H,H,A,H,A,H,H,A,A,A,-,H,H,H,A,A,A,-,H,H,A,H,A,H,A,A,A,H,A,A,H,H,A,A,A,-,A,H,-,A,-,A,H,-,H,H,H,A,-,A,A,A,A,A,H,H,A,H,A,H,H,H,A,A,H,A,H
+C1_m51,H,A,H,A,H,A,A,A,A,A,H,H,H,H,H,H,A,A,H,H,A,H,A,H,H,A,-,A,-,H,H,H,A,A,A,H,H,H,A,H,A,H,A,A,A,H,A,A,H,H,A,A,A,H,A,H,A,A,A,A,H,H,-,H,H,A,H,A,A,A,A,A,H,H,A,H,A,H,H,H,A,-,H,A,H
+X2_m12,H,A,H,A,H,A,A,A,A,A,H,H,H,H,H,H,A,A,H,H,A,H,A,H,H,A,A,A,H,H,A,H,A,A,A,H,H,H,A,H,A,H,A,A,A,H,A,A,H,H,A,A,A,H,A,H,A,A,A,A,A,H,H,H,H,A,H,A,A,A,A,A,H,H,A,A,A,H,H,H,A,A,H,A,H
+C3_m13,H,A,H,A,H,-,A,A,A,A,H,H,A,H,H,H,A,A,-,A,A,H,A,H,H,A,A,A,H,H,A,H,A,A,A,H,H,H,A,H,A,H,A,A,A,H,A,A,H,H,A,A,A,H,A,H,-,A,H,A,A,H,H,H,H,A,H,A,A,A,A,A,H,H,A,A,A,H,H,H,A,A,H,A,H
+C3_m23,H,A,H,A,H,-,A,A,A,A,H,H,A,H,H,H,A,A,-,A,A,H,A,H,H,A,A,A,-,H,A,H,A,A,A,H,H,H,A,H,A,H,A,A,A,H,A,A,H,-,A,A,A,H,-,H,A,A,A,A,A,H,H,H,H,A,H,A,A,A,A,A,H,H,A,-,A,H,H,H,A,A,H,A,H
+C3_m33,H,A,H,A,H,-,A,A,A,A,H,H,A,H,H,H,A,A,-,A,A,H,A,H,H,A,A,A,H,-,A,H,A,A,A,H,H,H,A,H,A,H,A,A,A,H,A,A,-,-,A,A,A,H,-,H,A,A,A,-,A,H,H,H,H,A,H,A,A,A,A,A,H,-,A,A,A,H,H,H,A,A,H,A,H
+C3_m43,H,A,H,A,H,-,A,A,A,A,H,H,A,H,H,H,A,A,-,A,A,H,A,H,H,A,A,A,H,H,A,H,A,A,A,H,H,H,A,H,A,H,A,A,A,H,A,-,H,A,H,A,A,H,H,H,A,A,A,A,A,H,H,H,H,A,H,A,A,A,A,A,H,H,A,A,A,H,H,H,A,A,H,A,H
+X4_m14,H,A,H,A,H,A,A,A,A,A,H,H,A,H,H,H,A,A,H,H,A,H,A,H,H,A,A,A,H,H,A,H,A,A,A,-,H,H,A,-,A,H,A,A,A,H,A,A,H,H,A,A,A,H,A,H,A,A,A,-,A,H,H,H,H,A,H,A,A,A,A,A,H,H,A,A,A,H,H,A,-,A,H,A,H
+X4_m24,H,A,-,A,-,A,-,A,A,A,-,H,-,H,H,H,A,A,H,H,A,H,A,H,H,A,A,A,H,H,A,H,A,A,A,H,H,H,A,-,A,H,A,A,A,H,A,A,H,H,A,A,A,H,A,H,A,-,A,-,A,H,H,H,H,A,H,A,A,A,A,A,H,H,A,A,A,H,H,A,A,A,H,A,H
+S5_m15,H,A,H,A,H,A,A,A,A,A,H,H,A,H,-,H,A,A,A,H,A,H,A,H,H,A,A,A,H,H,H,H,A,A,A,H,H,H,A,H,A,-,A,A,-,H,A,A,H,H,A,A,A,H,A,H,A,A,A,A,A,H,H,H,H,A,H,A,A,A,A,A,H,H,A,A,A,H,H,A,A,A,H,A,H
+X6_m16,H,A,H,A,H,A,A,A,A,A,H,H,A,H,H,H,A,-,H,H,A,H,A,H,H,A,A,A,H,H,H,H,A,A,A,H,H,H,A,H,A,H,A,A,-,H,A,A,H,H,A,A,A,H,H,H,A,A,A,A,A,H,H,H,H,H,H,A,A,A,A,A,H,H,A,A,A,H,H,-,A,A,H,A,H
+X6_m26,H,A,H,A,H,-,A,A,-,A,H,H,A,-,H,H,A,-,H,H,A,H,A,H,H,A,A,A,H,H,H,H,A,A,-,H,-,H,-,H,A,-,A,A,-,H,A,A,H,H,A,A,A,H,H,H,A,A,-,A,A,H,H,H,-,H,H,A,A,-,A,-,H,-,A,A,A,H,-,A,A,A,H,A,H
+X6_m36,H,A,H,A,H,A,A,A,A,A,H,-,A,H,H,H,A,-,H,H,A,H,A,H,H,A,A,A,H,H,H,H,A,A,A,H,H,-,A,H,A,H,A,A,-,H,A,A,H,H,A,A,A,H,H,H,A,A,A,A,A,H,-,H,H,H,H,A,A,A,A,A,H,H,A,A,A,H,H,A,A,A,H,A,H
+X6_m46,H,A,H,A,H,A,A,A,A,A,H,H,A,H,H,H,A,-,H,H,A,H,A,H,H,A,A,A,H,H,H,H,A,A,A,H,H,H,A,H,A,-,A,A,-,H,A,A,H,H,A,A,A,H,H,H,A,A,A,A,-,H,H,H,H,H,H,A,A,A,A,A,H,H,A,A,A,H,H,A,A,A,H,A,H
+S7_m17,H,A,H,A,H,A,A,A,A,A,H,H,A,H,H,H,A,A,H,-,A,H,A,H,H,A,A,A,H,H,A,H,A,A,A,H,H,H,A,H,A,H,A,A,A,H,A,A,H,H,A,A,A,H,A,H,A,A,A,A,A,H,H,H,H,H,H,A,A,-,A,A,H,H,A,A,A,H,H,H,A,A,H,A,H
+S8_m18,H,A,H,H,H,A,A,A,A,H,H,H,A,A,H,H,A,A,H,H,A,H,A,H,H,A,A,A,H,H,A,A,A,A,A,H,H,H,A,H,A,H,H,H,A,H,A,A,H,H,A,A,H,H,A,H,A,A,A,A,A,H,H,H,H,H,H,A,H,A,A,A,H,H,A,A,A,H,H,H,H,A,H,A,A
--- phase_inference.py
+++ phase_inference.py
@@ -9,14 +9,68 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--phase", type=str, help="phase file for the linkage group, created using genovect-batch, see README.txt for format notes")
 parser.add_argument("-c", "--clusters", type=str, help="cluster-tab-marker list for all markers in the dataset")
+parser.add_argument("-n", "--num_progeny", type=int, help="number of progeny in the family")
 args = parser.parse_args()
 
 
+class cluster: 
+	"""this class represents a cluster location in the marker order """
+	"""within these clusters, the initial imputation will take place"""
+	"""a consensus phase vector for the cluster will be output, and """
+	"""it may still include missing genotypes, where no data present"""
+	def __init__(self, cluster_name):
+		""" initiate, set name of cluster """
+		self.name = cluster_name
+	def members(self, cluster_dataframe):
+		""" grab a list of the member markers from the cluster df"""
+		""" I'm coding storage 2 ways here, not sure which will be"""
+		""" more efficient, delete if redundant"""
+		self.member_markers = cluster_dataframe[ cluster_dataframe['cluster'] == self.name ]
+		self.marker_list = list(self.member_markers['marker']) #not used yet
+	def phase_dat(self, phase_dataframe)
+		"""grab the phase data for the markers in the cluster"""
+		self.phase = phase_dataframe[ phase_dataframe['marker'].isin(self.member_markers['marker'])]
+		self.phase = self.phase.replace('-', np.nan)
+	def consensus_phase(self):
+		""" take the mode of the columns, changing the marker column to the cluster's name"""
+		self.consensus = self.phase.mode()
+		self.consensus['marker'] = self.name
+		#here exception handling is needed
+			#1. what if no values? --> make it an NaN
+			#2. if a tie... make it an NaN? then the next steps can impute it through comparison
+
+##test code##
 
 
-
+phase = phase_dataframe[ phase_dataframe['marker'].isin(member_markers['marker'])]
 
 
 
 if __name__ == '__main__':
+	""" turn the number of progeny into a list of names"""
+	progeny_head = ['P_%s' % ( d+1) for d in range(0,args.num_progeny)]
+	df_header = ['marker']
+	df_header.extend(progeny_head)
+	
+	"""read in the phase data """
+	phase_dat = pd.read_csv(args.phase, names=df_header)
 
+	"""read in the cluster designations """
+	cluster_dat = pd.read_csv(args.clusters)
+
+
+######## for development
+	num_progeny = 85
+	progeny_head = ['P_%s' % ( d+1) for d in range(0,num_progeny)]
+
+	df_header = ['marker']
+	df_header.extend(progeny_head)
+
+	phase_dat = pd.read_csv('example_phase_hq.f', names=df_header)
+	cluster_dat = pd.read_table('cluster_members.txt')
+
+
+
+
+
+	
